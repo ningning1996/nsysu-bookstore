3f1339c99c0cc44b10b69f32f043e239533acc73
ningning1996 <39486721+ningning1996@users.noreply.github.com>
2019-11-14 22:36:35 +0800

    Add files via upload

---
+++ Models/CourseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using PagedList;
+
+
+namespace 宏盈.Models
+{
+ 
+        public class CourseRetrieveViewModel
+        {
+            public IPagedList<COURSE> courses { get; set; }
+            public IPagedList<DEPARTMENT> departments { get; set; }
+
+            
+        }
+
+        public class BuybookViewModel
+        {   
+            public COURSE course { get; set; }
+            public List<BUYBOOK> Buybooks { get; set; }
+            public BUYBOOK Buybook { get; set; }
+        }
+        public class BuybookUpdateViewModel
+        {   
+            public BUYBOOK Buybook { get; set; }
+        }
+}
